bbc9e465f500ed12b9ee4f424ba469747f8dc9c4
MarieSophieDuplouich <ms.duplouichiscod@gmail.com>
2025-09-26 15:43:52 +0200

    exo 2 abonne manque booléen

--- 2 - abonne/script.js
+++ 2 - abonne/script.js
@@ -10,12 +10,77 @@ Nouvelles compétences:
     * replace
 */
 
+
+// quand je clique sur le smiley blanc il devient jaune -> html et css ->
 // 1. Je recupere l'icone smiley
 
 // 2. Je recupere le bouton ABONNER
 
-// 3. Je défini une variable binaire qui exprime l'etat abonné ou non de l'utilisateur
+// quand je clique sur le bouton abonner, le bouton abonner change de coueleur et le texte change en "abonné"-> contenu->InnerText
 
-// 4. J'ecoute le clique sur l'icone smiley
+// 3. Je défini une variable binaire qui exprime l'etat abonné ou non de l'utilisateur -> booleén
 
-// 5. J'ecoute l'evenemment click sur le bouton ABONNEZ
+// 4. J'ecoute le clique sur l'icone smiley ->addeventlistener click
+
+// 5. J'ecoute l'evenemment click sur le bouton ABONNEZ ->addeventlistener click
+
+
+//Sélectionner
+
+const smileyMeh = document.querySelector(".fa-solid.fa-face-meh");
+const smileyHappy = document.querySelector(".fa-solid fa-face-smile");
+const btnBtnaccept = document.querySelector(".bloc-btn");
+const btnBtnabonnerchangecolor= document.querySelector(".btn-sub");
+const btnBtnabonnerchangetext= document.querySelector(".btn-sub");
+
+const booleanUser = false;
+// function smiley
+
+function likeSmiley(){
+    
+smileyMeh.classList.add("happy");
+
+}
+
+
+// function abonner vous
+
+
+
+// function abonner change de couleur abonnez vous
+function btnAbonnerchangecolor() {
+    btnBtnabonnerchangecolor.classList.add("abonne");
+}
+
+
+function action(){
+//    btnBtnabonnerchangetext.classList.toggle('hidden');
+    btnBtnabonnerchangetext.textContent = "Abonné";
+}
+    
+ // function bool etat ou non abonné ou non User  true false
+
+ function misClick(){
+if (hour < 15) {
+  greeting = "Good day!";
+} else {
+  greeting = "Good evening!";
+}
+
+ }
+
+
+
+
+
+// évènement
+
+btnBtnaccept.addEventListener('click',likeSmiley);
+btnBtnabonnerchangecolor.addEventListener('click', btnAbonnerchangecolor);
+btnBtnabonnerchangetext.addEventListener('click', action);
+
+// btnBtnabonnerchangetext.addEventListener('click', btnChangerTextabonner);
+// modifier DOM
+
+
+// fetch
