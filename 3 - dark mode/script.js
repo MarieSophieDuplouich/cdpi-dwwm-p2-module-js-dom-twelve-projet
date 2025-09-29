a1c83a4e2824c391ad5b089358fa490a0892ff17
MarieSophieDuplouich <ms.duplouichiscod@gmail.com>
2025-09-29 13:29:40 +0200

    probleme de push pour 2 bannière

--- 3 - dark mode/script.js
+++ 3 - dark mode/script.js
@@ -3,3 +3,55 @@ Nouvelles compétences:
 - forEach
 - querySelectorAll
 */
+
+
+// quand je clique sur le bouton toggleDarkmode, le site web devient NodeIterator, c'est le css quia change
+// donc -> contenu
+
+
+
+// Sélectionner
+
+const toggleDarkmode = document.querySelectorAll("*");
+const iconChange = document.querySelector(".fa-moon");
+const btnIbutton = document.querySelector(".switch");
+
+
+//Fonction
+
+function activerDarkmode(){
+toggleDarkmode.forEach(dark_elem=>{
+    // Les objet Element possède également la fonction querySelector.
+
+dark_elem.classList.toggle('darkmode');
+
+
+});
+
+}
+//évènements
+btnIbutton.addEventListener('click', activerDarkmode);
+
+
+//modifier DOM
+
+
+
+//Fetch
+
+
+// console.log(produits_arr[0]); // La div des Nike
+// console.log(produits_arr[1]); // La div des Adidas
+// console.log(produits_arr[2]); // La div des Puma
+// // // console.log(produits_arr.length);
+// const color =  document.getElementsByClassName(".myH2").style.background = "#ff0000";
+
+// const produits_arr = document.querySelectorAll("#produit"); 
+// produits_arr.forEach(produit_elem=>{
+//     // Les objet Element possède également la fonction querySelector.
+//     console.log(produit_elem.querySelector("p").innerText);
+//     produits_arr.forEach(produit_elem=>{
+        
+//        produit_elem.style.background = "lightblue";
+
+//  }) };
