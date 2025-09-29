de6acf4ac8d072f21ebf07853bef3809baaaeab2
MarieSophieDuplouich <ms.duplouichiscod@gmail.com>
2025-09-29 14:07:01 +0200

    exo 3 deuxième partie faite

--- 3.1 - dark mode mutiple boutons/script.js
+++ 3.1 - dark mode mutiple boutons/script.js
@@ -4,3 +4,38 @@
 //2. Je recupere tout les elements html de ma page
 
 //3. Je parcours tout les switchs
+
+// Sélection des éléments
+const allElements = document.querySelectorAll("*");
+const switchBtns = document.querySelectorAll(".switch"); // tous les boutons
+const icons = document.querySelectorAll(".icon");
+
+// Fonction pour activer/désactiver le dark mode
+function toggleDarkMode() {
+    allElements.forEach(el => {
+        el.classList.toggle('darkmode');
+    });
+
+    icons.forEach(icon => {
+        icon.classList.toggle('fa-moon'); // exemple : changer icône
+        icon.classList.toggle('fa-sun');
+    });
+}
+
+// Ajouter l'événement à chaque bouton
+switchBtns.forEach(btn => {
+    btn.addEventListener('click', toggleDarkMode);
+});
+
+
+//modifier DOM
+
+// remarque quand c'est un élément seul par icon moon pas besoin de foreach une fonction function.classList.toggle('fa-moon')
+// suffit amplement
+
+
+//Fetch
+
+
+
+
