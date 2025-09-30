2ec2cda6bfeac87446a440e88a280ab5375a27b2
MarieSophieDuplouich <ms.duplouichiscod@gmail.com>
2025-09-30 15:47:31 +0200

    exo 6 toast terminé

--- 6 - toast/script.js
+++ 6 - toast/script.js
@@ -11,3 +11,52 @@
 const btn = document.querySelector("button");
 // 2. Je recupere le conteneur de toasts
 const ctnToasts = document.querySelector(".container-toasts");
+//  ctnToasts.style.display ="none";
+//fonctions
+
+function printFichier() {
+
+
+const toast = document.createElement("div");
+
+toast.classList.add("toast");
+
+toast.innerHTML="Votre fichier est enregistré !";
+ctnToasts.appendChild(toast);// appenchild ici??
+
+
+
+setTimeout(function()
+{toast.remove();
+
+}, 2000);
+}
+// evenement
+btn.addEventListener('click', printFichier);
+
+
+
+
+
+//DOM
+
+
+//fetch
+
+
+// quand je clique sur enregistré le site web m'affiche votre fichier est enregistré -> contenu innerhtml
+
+
+
+
+// setTimeout (function(){
+//     console.log("Ouiiii je scroollll");
+//       modal.style.display ="flex";
+// }, 2000);
+// //Evenements
+// // 5. Je fais disparaite la modale au clique sur la croix
+// modalQuit.addEventListener("click",function(){
+    
+    
+
+// });
