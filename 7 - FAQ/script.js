4154578ff56d13878ece5fbd8845fd042e549dab
MarieSophieDuplouich <ms.duplouichiscod@gmail.com>
2025-09-30 11:31:08 +0200

    7FAQ fini

--- 7 - FAQ/script.js
+++ 7 - FAQ/script.js
@@ -7,28 +7,55 @@
  * 
  */
 
-// 1. Je recupere toutes les questions
-const questions = document.querySelectorAll(".question");
-
-// 2. Je parcours toutes les questions
-questions.forEach(function(question){
+// Séléctionner 
+const quesTions = document.querySelectorAll(".question");
+// Fonctions
+quesTions.forEach(function (question) {
     // 3. Lors du click sur la question
-    question.addEventListener("click",function(){
+    question.addEventListener("click", function () {
         
-        questions.forEach(item => {
+          
+        const rePonse = question.nextElementSibling;
+        const  bTn = question.querySelector(".btn-arrow");
+
+        quesTions.forEach(item => {
             if (item !== question) {
-                item.classList.remove('show-reponse'); 
-              
+                // item.classList.remove('show-reponse'); 
+                item.nextElementSibling.style.maxHeight = null; // Collapse the answer          
+                   const otherBtn = item.querySelector(".btn-arrow");
+                otherBtn.classList.remove("fa-chevron-up");
+                otherBtn.classList.add("fa-chevron-down");
             }
+          
         });
-
+          
         // Toggle 'active' class on the clicked question to rotate the arrow
-        question.classList.toggle('show-reponse');
-
-    
-  
+        rePonse.classList.toggle('show-reponse');
+         
+        // Check if the answer is already open
+        if (rePonse.style.maxHeight) {
+            // If open, close it by resetting max-height
+            rePonse.style.maxHeight = null;
+             bTn.classList.remove("fa-chevron-up");
+            bTn.classList.add("fa-chevron-down");
+          
+        
+        } else {
+            // If closed, set max-height to scrollHeight to expand it
+            rePonse.style.maxHeight = rePonse.scrollHeight + 'px';
+            bTn.classList.remove("fa-chevron-down");
+            bTn.classList.add("fa-chevron-up");
+        }
+   
     });
 });
+
+
+// Evenements
+
+//Modifier Dom
+
+//Fetch
 
 
 
