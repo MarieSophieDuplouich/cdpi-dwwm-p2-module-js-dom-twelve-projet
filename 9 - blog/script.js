edff99d924c5ddb39c31f66e75c6e3bcdaefb2a2
MarieSophieDuplouich <ms.duplouichiscod@gmail.com>
2025-10-01 12:00:11 +0200

    9 blog presque terminé

--- 9 - blog/script.js
+++ 9 - blog/script.js
@@ -31,16 +31,64 @@ const posts =
 ];
 
 // 3. Je parcours la bdd
+
+
 posts.forEach(function(post){
+      const div = document.createElement(".post");
+  
     // 4. Je créer un nouveau post dans le conteneur de post pour chaque post de la BDD
-    
+    const dIv = document.createElement("div")
+    // div.textContent = post.titre,post.hashtag,post.link,post.extrait;
+      dIv.textContent = post.titre;
+     dIv.classList.add("post-titre");
+
+
+     const diVe = document.createElement("div");
+     diVe.classList.add("post-extrait");
+      diVe.textContent = post.extrait;
+      const diVVe = document.createElement("div");
+     diVVe.classList.add("post-hashtag");
+     diVVe.textContent = post.hashtag;
+
+
+    //  div.textContent = post.link;
+      
+
     // 5. Je l'ajoute dans le conteneur de publication
-    post-titre
-    post-extrait
-    post-hashtag
+    dIv.appendChild(div);
+    diVe.appendChild(div);
+    diVVe.appendChild(div);
 
-    ce sont les classes
     
+     postsContainer.appendChild(div);
 
 
-});
+}); 
+
+
+   
+            // <div class="post">
+            //     <!-- <div class="post-titre">
+            //         <a href="lien">Titre de l'article</a>
+            //     </div>
+            //     <div class="post-extrait">
+            //         Extrait
+            //     </div>
+            //     <div class="post-hashtag">
+            //         #tag
+            //     </div> -->
+            // </div>
+
+
+
+// const fruits = ['apple', 'banana', 'cherry'];
+// const outputDiv = document.getElementById("output");
+
+// fruits.forEach((fruit) => {
+//   // Je crée un paragraphe pour chaque fruit
+//   const p = document.createElement("p");
+//   p.textContent = `Fruits: ${fruit}`;
+  
+//   // J'ajoute ce paragraphe dans la div
+//   outputDiv.appendChild(p);
+// });
