96ede476a1de45e085e98ae65e042373b6a413a3
MarieSophieDuplouich <ms.duplouichiscod@gmail.com>
2025-10-01 09:34:10 +0200

    8 fiche produit commencé

--- 8 - Fiche produit/script.js
+++ 8 - Fiche produit/script.js
@@ -6,6 +6,8 @@
  * operateur ternaire
  */
 
+
+// Séléctionner
 // 1. Je recupere toutes les petites vignettes
 const vignettes = document.querySelectorAll(".small");
 // 2. Je recupere la grande photo
@@ -19,4 +21,34 @@ const panierMsg = document.querySelector(".panier-msg");
 // 5. Je crée une variable qui contient le nombre de produits ajoutées ua panier
 let panier = 0;
 
-// ...
+//Fonctions
+
+
+// quand je clique sur une image elle devient  grosse
+function imageDevientgrosse{
+
+
+}
+
+
+
+
+// quand je clique sur le bouton ajouter Panier une phrase s'affiche avec "Vous avez nombredeproduits produits dans votre panier."
+
+
+function panierAugmente(){
+          
+
+
+
+    "Vous avez"+numberproduct+"produits dans votre panier.";
+
+
+}
+
+//évenments
+
+//DOM
+
+
+//Fetch
