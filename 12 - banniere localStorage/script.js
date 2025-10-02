7f4181b89ac1f67e79955976b00e8596ccc9200a
MarieSophieDuplouich <ms.duplouichiscod@gmail.com>
2025-10-02 13:24:57 +0200

    12 localstorage bannière fini ne pas faire le 11 todolist

--- 12 - banniere localStorage/script.js
+++ 12 - banniere localStorage/script.js
@@ -7,53 +7,52 @@
 
 // sélectionner
 
-// le btn et les cookies pour les faire disparaître
-// MS Toggle
-// function garderChoixusercookies() {
-//     document.body.classList.toggle("btn-accept");
+const cookies = document.querySelector(".cookies");
+const btnBtnaccept = document.querySelector(".btn.btn-accept");
+function garderChoixusercookies() {
+    document.body.classList.toggle("btn-accept");
 
-//     if (document.body.classList.contains("btn-accept")) {
-//         localStorage.setItem("theme", "dark");
-//         if (logo) {
-           
-//         }
-//     } else {
-//         localStorage.setItem("theme", "light");
-//         if (logo) {
-           
-//         }
-//     }
+    if (document.body.classList.contains("btn-accept")) {
+        localStorage.setItem("cookies", "accepted");
+        if (cookies) {
+          cookies.classList.add("cookies-display-none");
+        }
+    } 
+    console.log("cookies acceptés");
+}
 
-//     console.log(garderChoixusercookies());
-// }
-
-// document.addEventListener("DOMContentLoaded", function () {
-//     const toggle = document.getElementById("toggle");
-//     const savedTheme = localStorage.getItem("theme");
-
-//     if (savedTheme === "dark") {
-//         document.body.classList.add("btn-accept");
-//         if (toggle) toggle.checked = true;
+document.addEventListener("DOMContentLoaded", function () {
+    const savedCookies = localStorage.getItem("cookies");
+        
+    // cookies.classList.remove('cookies');
+    if (savedCookies === "accepted") {
        
-//     }
-// });
+        if (cookies) cookies.style.display = "none";
+    }
+
+     console.log("cookies déjà acceptés");
+});
+
+
+btnBtnaccept.addEventListener('click',garderChoixusercookies);
+
 
 // document.cookie = "username=JohnDoe; expires=Fri, 31 Dec 2025 23:59:59 GMT; path=/";
 // console.log(document.cookie); // Output: username=JohnDoe
 // document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 
-function setAuthToken(token) {
-    document.cookie = `authToken=${token}; path=/; expires=Fri, 31 Dec 2025 23:59:59 GMT`;
-}
+// function setAuthToken(token) {
+//     document.cookie = `authToken=${token}; path=/; expires=Fri, 31 Dec 2025 23:59:59 GMT`;
+// }
 
-function getAuthToken() {
-    let cookies = document.cookie.split("; ");
-    for (let cookie of cookies) {
-        let [name, value] = cookie.split("=");
-        if (name === "authToken") return value;
-         console.log(cookies);
-    }
-    return null;
+// function getAuthToken() {
+//     let cookies = document.cookie.split("; ");
+//     for (let cookie of cookies) {
+//         let [name, value] = cookie.split("=");
+//         if (name === "authToken") return value;
+//          console.log(cookies);
+//     }
+//     return null;
 
    
-}
+// }
